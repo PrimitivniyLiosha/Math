c716e465060472908a417bdaa8a82921737926a8
Frolov Alexey <lex.frol2020@gmail.com>
2019-02-18 22:31:59 +0200

    3 methods on the mathematics

---
+++ priamougolnik.js
@@ -0,0 +1,26 @@
+ let priampugolnik = (a, b, n) => {
+    let x = a;
+    let fx = x * x;
+    let s = 0;
+    let h = (b - a) / n;
+    for (let i = a; i <= b; i++) {
+        if (x <= b) {
+            s += fx;
+            x += x + h;
+        } else {
+            s = h * s;
+        }
+    }
+    console.log(s);
+}
+priampugolnik(2, 5, 4);
+
+
+
+
+
+
+
+
+
+
